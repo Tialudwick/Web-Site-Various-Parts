e869eebf6d8d20740367da9e42f31ed879ab4f1c
Orion Ludwick <Tia.ludwick@Maine.edu>
2026-02-18 15:21:53 +0000

    updates

--- Bootstrap/JS/js.js
+++ Bootstrap/JS/js.js
@@ -1,7 +1,14 @@
-<script>
+
   window.addEventListener("scroll", () => {
     const hero = document.querySelector(".hero");
     const scroll = window.scrollY;
     hero.style.backgroundPositionY = `${scroll * 0.3}px`;
   });
-</script>
+
+
+
+
+  window.addEventListener("scroll", () => {
+    const nav = document.getElementById("mainNav");
+    nav.classList.toggle("scrolled", window.scrollY > 50);
+  });
